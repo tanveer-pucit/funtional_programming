24e932beb5d7d992431e8edde25aded4031b2a84
Tanveer Mahmood <tanveer@BilalBaig-Desktop-78.arbisoft.com>
2018-12-10 12:50:11 +0500

    update my_map by by separate branch

--- my_reduce.py
+++ my_reduce.py
@@ -2,5 +2,7 @@ from functools import reduce
 
 # Reduce takes a function and a collection of items. It returns a value that is created by combining the items.
 
+# this comment line for checking the branch work only
+
 summ = reduce(lambda a, vaiable: a + vaiable, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
 print(summ)
